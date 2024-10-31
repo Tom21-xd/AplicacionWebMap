0b2b36355c0f064fe3b9c510d5900c5571cc0434
tom21xd <johans.ramirez@udla.edu.co>
2024-10-31 03:04:18 -0500

    Cadena de conexion

--- AplicacionWebMap/Data/Conexion.cs
+++ AplicacionWebMap/Data/Conexion.cs
@@ -1,7 +1,35 @@
-﻿namespace AplicacionWebMap.Data
+﻿using Npgsql;
+using System.Data;
+
+
+namespace AplicacionWebMap.Data
 {
     public class Conexion
     {
+        private readonly string _connectionString;
 
+        public Conexion(IConfiguration configuration)
+        {
+            // Lee la cadena de conexión desde appsettings.json
+            _connectionString = configuration.GetConnectionString("PostgresConnection");
+        }
+
+        // Método para obtener la conexión
+        public NpgsqlConnection GetConnection()
+        {
+            var connection = new NpgsqlConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+
+        // Método para cerrar la conexión de forma segura
+        public void CloseConnection(NpgsqlConnection connection)
+        {
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
     }
+
 }
